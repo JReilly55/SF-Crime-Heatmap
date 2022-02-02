1455da411061e86f3eee9473d50eca0c1a0b7675
JReilly55 <98059081+JReilly55@users.noreply.github.com>
2022-02-02 15:53:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 var mymap = L.map('mapid').setView([37.7739, -122.4194], 12);
 
-var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-	minZoom: 12,
-  maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
 
